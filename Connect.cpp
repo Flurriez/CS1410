e8504286fa71358efa115433dc2a22da84983e62
Flurriez <skabelanator@live.com>
2014-01-23 10:40:00 -0700

    First commit of Connect.cpp

---
+++ Connect.cpp
@@ -0,0 +1,10 @@
+//Kyle Skabelund
+//Cs1410
+
+#include "Connect.h"
+
+int GetConnectonSize(void){
+	int connectionSize;
+	cout << "How many connections are required to win the game?" << endl;
+	cin >> conectionSize;
+}
