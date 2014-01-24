ea6a4ed90a1e958e72b769c31e2c0c9258658c99
Flurriez <skabelanator@live.com>
2014-01-24 11:43:37 -0700

    Updated Connect.cpp

--- Connect.cpp
+++ Connect.cpp
@@ -3,8 +3,48 @@
 
 #include "Connect.h"
 
+const int EXTRA_ROWS;
+
 int GetConnectonSize(void){
 	int connectionSize;
 	cout << "How many connections are required to win the game?" << endl;
-	cin >> conectionSize;
+	cin >> connectionSize;
+}
+char **BuildGameBoardSize(int winSize){
+
+	char **Gameboard = new char *[winSize + EXTRA_ROWS];
+
+}
+void DisplayGameBoard(char **GameBoard, int winSize){
+
+}
+void PlayerMove(char **GameBoard, int winsize){
+
+}
+void ComputerMove(char **GameBoard, int winsize){
+
+}
+
+bool CheckColumnWin(char **Gameboard, int winsize){
+
+}
+
+bool CheckRowWin(char **Gameboard, int winsize){
+
+}
+
+bool CheckLeftDiagonalWin(char **Gameboard, int winsize){
+
+}
+
+bool CheckRightDiagonalWin(char **Gameboard, int winsize){
+
+}
+
+bool CheckTie(char **Gameboard,int winsize){
+
+}
+
+void DestroyGameboard(char **Gameboard, int winsize){
+
 }
